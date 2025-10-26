f657df0c56ea00f8de8fe8204e68243ebdc89012
Mohammed <mohammedtaher6620@gmail.com>
2025-10-26 21:02:07 +0300

    add

--- src/components/Requests/ServiceRequestForm.jsx
+++ src/components/Requests/ServiceRequestForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const ServiceRequestForm = () => {
   const [title, setTitle] = useState('');
@@ -106,6 +107,7 @@ export const ServiceRequestForm = () => {
 
           <div className="pt-2">
             <button
+              onClick={() => {Navigate ('/client/request')} }
               type="submit"
               className="inline-flex items-center rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
